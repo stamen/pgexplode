1deb2df62856ecf37912269e1399431e4fce29c8
Seth Fitzsimmons <seth@mojodna.net>
2018-02-13 14:29:03 -0800

    Escape $s in passwords

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ if (process.env.DATABASE_URL) {
     env.PGPORT = cfg.port;
     env.PGDATABASE = cfg.database;
     env.PGUSER = cfg.user;
-    env.PGPASSWORD = cfg.password;
+    env.PGPASSWORD = cfg.password.replace("$", "\\$");
 
     env.PGSSLMODE = cfg.ssl ? "require" : "";
     env.PGAPPNAME = cfg.application_name;
